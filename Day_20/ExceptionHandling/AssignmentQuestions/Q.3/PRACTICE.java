d560dd3b523284c82323bd5f83d9bef146e19083
rajeshreepalve <127953521+rajeshreepalve@users.noreply.github.com>
2023-04-07 14:46:45 +0530

    Update PRACTICE.java

--- Day_20/ExceptionHandling/AssignmentQuestions/Q.3/PRACTICE.java
+++ Day_20/ExceptionHandling/AssignmentQuestions/Q.3/PRACTICE.java
@@ -1,3 +1,21 @@
+/**
+Q4) Write a program that accepts 2 integers a and b as input and finds the quotient of a/b. This program may generate an Arithmetic Exception. Use exception handling mechanisms to handle this exception. In the catch block, print the message as shown in the sample output. also, illustrate the use of the finally block. Print the message “Inside finally block” 
+Sample Input and Output 1: 
+Enter the 2 numbers 5 2 
+The quotient of 5/2 = 2 
+Inside finally block 
+
+Sample Input and Output 2: 
+Enter the 2 numbers 5 0 DivideByZeroException caught 
+Inside finally block 
+*/
+
+
+
+
+
+
+
 package com.Day_20_Exception_Handling;
 
 import java.util.Scanner;
